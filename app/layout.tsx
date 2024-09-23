22b8da9ec58fb0afb2333cb962693a497f9e9030
nathan-g1 <nathygech111@gmail.com>
2024-09-22 22:57:07 -0500

    New version

---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import './global.css'
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import { Navbar } from './components/nav'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import Footer from './components/footer'
+import { baseUrl } from './sitemap'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Nathan Lechamo - Software Engineer | Budding Researcher',
+    template: '%s | Next.js Portfolio Starter',
+  },
+  description: 'Welcome to my website, a passionate Software Engineer.',
+  openGraph: {
+    title: 'Nathan Lechamo - Software Engineer Portfolio',
+    description: 'Welcome to my website, a passionate Software Engineer.',
+    url: baseUrl,
+    siteName: 'Nathan Lechamo',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+}
+
+const cx = (...classes) => classes.filter(Boolean).join(' ')
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        My Portfolio
+      </h1>
+
+      {/* Put a next js image using tailwind from source app/images/image.jpg */}
+
+      <div className="w-32 h-32 mx-auto mb-8 relative">
+        <Image
+          className="rounded-full"
+          src="https://media.licdn.com/dms/image/v2/D5603AQHHJycH7b-pNw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724863346791?e=1732752000&v=beta&t=hEVAHB6CMtmfXkkmPx1HZBDW5DacMulBZS10mRmsdAM"
+          alt="Nathan Lechamo"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <p className="mb-4">
+        {`I'm Nathan Lechamo, a skilled software engineer with experience in machine learning, 
+        computer vision, and blockchain technologies. During my work as a Graduate Research 
+        Assistant, I developed a robust defense framework using TensorFlow to detect and mitigate 
+        adversarial patch attacks on deep learning models. This work leveraged GPU-based parallel 
+        infrastructure and Apache Spark, resulting in significant improvements in computer vision 
+        tasks. My research demonstrated better performance in patch detection and image restoration.`}
+      </p>
+
+      <p className="mb-4">
+      {`
+        In my professional career, I have made significant contributions 
+        to various organizations. At SpaceShard, I implemented optimal 
+        approaches to integrate blockchain utility functions into the 
+        Starknet-hardhat-plugin and constructed an efficient smart contract 
+        re-compiler with caching capabilities. My meticulous design and 
+        execution of over 100 unit tests and 30 integration tests ensured 
+        a seamless product rollout with approximately 90% code coverage. 
+        My diverse skill set includes Python, TypeScript, Node.js, and 
+        AWS, making me a versatile and valuable asset to any team.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
