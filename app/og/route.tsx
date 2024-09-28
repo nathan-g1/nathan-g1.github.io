0b304ca5953f910160093ee9a3f20a40591ecd32
nathan-g1 <nathygech111@gmail.com>
2024-09-27 21:44:04 -0500

    Fix issue on deploying

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,22 +1,34 @@
 import { ImageResponse } from 'next/og'
+import { isDynamicServerError } from 'next/dist/client/components/hooks-server-context';
 
 export function GET(request: Request) {
-  let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+  try {
+    let url = new URL(request.url)
+    let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
 
-  return new ImageResponse(
-    (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
+    return new ImageResponse(
+      (
+        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+            <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+              {title}
+            </h2>
+          </div>
         </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    )
+  } 
+  catch (error) {
+    if (isDynamicServerError(error)) {
+      throw error;
     }
-  )
+
+    // Handle other errors
+    console.error('An error occurred:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
